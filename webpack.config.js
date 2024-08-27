73ff4a4414dc35f1678d62cd78ff7ecd9942b3e3
abnsol <abenisolo51196@gmail.com>
2024-08-27 22:20:15 +0300

    add dependancy file

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode : "development",
+    entry : "./src/js/index.js",
+    output :  {
+        filename : "main.js",
+        path: path.resolve(__dirname,"dist"),
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/page/template.html"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/page/template.html" 
+        })
+    ],
+    module: {
+        rules:[
+            {
+                test:/\.css$/i,
+                use: ["style-loader","css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+}
