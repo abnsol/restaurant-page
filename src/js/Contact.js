bb8a2139e73e97336d8396d97510bc07a42f1c52
abnsol <abenisolo51196@gmail.com>
2024-08-27 22:15:57 +0300

    add content loader for home tab

---
+++ src/js/Contact.js
@@ -0,0 +1,5 @@
+import "../styles/contact.css";
+
+export function contactDomLoad(){
+    
+}
---
+++ src/js/homeDom.js
@@ -0,0 +1,41 @@
+import "../styles/style.css";
+
+export function domLoad(){
+    const content = document.querySelector("#content");
+    while (content.firstChild) {
+        content.removeChild(content.firstChild);
+    }
+
+    const home = document.createElement("div");
+    home.setAttribute("class","home");
+
+    const logoBackground = document.createElement("div");
+
+    const welcome = document.createElement("div");
+    welcome.textContent = "Welcome to";
+    welcome.setAttribute("id","welcome");
+
+    const logo = document.createElement("div");
+    logo.textContent = "WAKANDA RESTAURANT";
+    logoBackground.appendChild(welcome);
+    logoBackground.appendChild(logo);
+
+    home.appendChild(logoBackground);
+
+    const discover = document.createElement("div");
+    discover.setAttribute("class","discover");
+
+    const discoverParagraph = document.createElement("div");
+    discoverParagraph.textContent = "Welcome to our African-style restaurant, where every dish is a journey through the vibrant and diverse flavors of Africa. We bring you authentic recipes passed down through generations, using fresh, locally-sourced ingredients to create a unique culinary experience. Our warm and inviting atmosphere, infused with rich African culture, makes it the perfect place to gather with friends and family. Come and savor the true taste of Africa, where tradition meets creativity on every plate."
+
+    const discoverPic = document.createElement("div");
+    discoverPic.setAttribute("id","pic");
+
+    discover.appendChild(discoverParagraph);
+    discover.appendChild(discoverPic);
+
+    content.appendChild(home);
+    content.appendChild(discover);
+}
+
+
